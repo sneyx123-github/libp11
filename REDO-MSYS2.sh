dca4e4f40a0ff019db5f013df7aa7d893fdb1769
Simon Boris C. Ney <simonboriscney@gmail.com>
2024-09-09 15:03:40 -0700

    dito

--- REDO-MSYS2.sh
+++ REDO-MSYS2.sh
@@ -1,4 +1,127 @@
 export PATH=/usr/bin:$PATH
 export PKG_CONFIG_PATH=/mingw64/lib/pkgconfig:$PKG_CONFIG_PATH
 
+if [ 0 -eq 1 ]; then
+	LIBS="-lssl -lcrypto" LDFLAGS="-L/mingw64/lib" CFLAGS="-I/mingw64/include" ./configure
+	make
+	make install
+fi
+
+if [ 0 -eq 1 ]; then
+	cp src/.libs/pkcs11.dll /usr/lib/openssl/engines-3/
+fi
+if [ 0 -eq 0 ]; then
+	openssl engine pkcs11 -t
+fi
+
+if [ 0 -eq 1 ]; then
+	cat <<-END > x3.cnf
+	openssl_conf = openssl_init
+	
+	[openssl_init]
+	engines = engine_section
+	
+	[engine_section]
+	pkcs11 = pkcs11_section
+	
+	[pkcs11_section]
+	engine_id = pkcs11
+	dynamic_path = /usr/lib/openssl/engines-3/pkcs11.dll 
+	MODULE_PATH = /c/Program Files/Yubico/Yubico PIV Tool/bin/libykcs11.dll
+	PIN = 654321
+	END
+elif [ 0 -eq 0 ]; then
+	cat <<-END > x3.cnf
+	openssl_conf = openssl_init
+	
+	[openssl_init]
+	engines = engine_section
+	
+	[engine_section]
+	pkcs11 = pkcs11_section
+	
+	[pkcs11_section]
+	engine_id = pkcs11
+	dynamic_path = pkcs11.dll
+	MODULE_PATH = /c/Program Files/Yubico/Yubico PIV Tool/bin/libykcs11.dll
+	PIN = 654321
+	END
+fi
+
+
+if [ 0 -eq 0 ]; then
+	openssl engine dynamic \
+		-pre "SO_PATH:/usr/lib/openssl/engines-3/pkcs11.dll" \
+		-pre "ID:pkcs11" \
+		-pre "LIST_ADD:1" \
+		-pre "LOAD" \
+		-pre "MODULE_PATH:/c/Program Files/Yubico/Yubico PIV Tool/bin/libykcs11.dll"
+fi
+
+if [ 0 -eq 1 ]; then
+	openssl req \
+		-engine pkcs11 \
+		-new \
+		-key "pkcs11:object=test-key;type=private;pin-value=654321" \
+		-keyform engine \
+		-out req.pem \
+		-text \
+		-x509 \
+		-subj "/CN=Simon Y. Ney" \
+		-verbose
+fi
+if [ 0 -eq 1 ]; then
+	OBJ_LABEL="test-key"
+	OBJ_LABEL="X.509 Certificate for PIV Authentication"
+	OBJ_LABEL="Authentication"
+	openssl x509 \
+		-engine pkcs11 \
+		-signkey "pkcs11:object=$OBJ_LABEL;type=private;pin-value=654321" \
+		-keyform engine \
+		-in req.pem \
+		-out cert.pem
+fi
+if [ 0 -eq 1 ]; then
+	OBJ_LABEL="test-key"
+	OBJ_LABEL="X.509 Certificate for PIV Authentication"
+	OBJ_LABEL="Authentication"
+	openssl pkeyutl \
+		-engine pkcs11 \
+		-keyform engine \
+		-inkey "pkcs11:object=$OBJ_LABEL;type=private" \
+		-sign \
+		-in data.txt \
+		-out signature.bin
+fi
+if [ 0 -eq 1 ]; then
+	cp $dp/xykca_certificate.pem .
+	cp $dp/xykca_public_key.pem .
+	cp $dp/xykca_data.txt .
+	cp $dp/xykca_data_signature.bin .
+fi
+
+if [ 0 -eq 1 ]; then
+	openssl x509 \
+		-in xykca_certificate.pem \
+		-pubkey \
+		-out xykca_public_key.pem 
+fi
+
+if [ 0 -eq 1 ]; then
+	openssl dgst \
+		-verify xykca_public_key.pem \
+		-signature xykca_data_signature.bin \
+		xykca_data.txt
+fi
+
+if [ 0 -eq 1 ]; then
+	openssl <<- END
+	req -engine pkcs11 -new -key "pkcs11:object=test-key;type=private;pin-value=XXXX" \
+			-keyform engine -out req.pem -text -x509 -subj "/CN=Simon Y. Ney"
+	x509 -engine pkcs11 -signkey "pkcs11:object=test-key;type=private;pin-value=XXXX" \
+			-keyform engine -in req.pem -out cert.pem
+	END
+fi
+
 # does G_CB works here?
+
---
+++ bash_for_libp12.ps1
@@ -0,0 +1,2 @@
+# Entry from Host to msys2 console for p12 compilation.
+C:\msys64\usr\bin\bash.exe
